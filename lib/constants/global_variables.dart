23fce60ad21f84737a3ff7ed021045ac9c751192
VuongVladimir <125279491+VuongVladimir@users.noreply.github.com>
2024-10-02 19:36:19 +0700

    upload vercel

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = "http://10.128.125.137:3000";
+String uri = "http://192.168.1.11:3000";
 class GlobalVariables {
   // COLORS
   static const appBarGradient = LinearGradient(
